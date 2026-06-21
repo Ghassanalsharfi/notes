e06b830e3eca99246e20a6ddcfd7d1bc8deba673
My project <g2036535@gmail.com>
2026-06-21 23:08:16 +0300

    Create CustomSearchIcon class

---
+++ lib/widgets/custom_search_icon.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+ 
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 46,
+      width: 46,
+      decoration: BoxDecoration(color: Colors.white),
+      child: Center(child: Icon(Icons.search,size: 28,)),
+    );
+  }
+}
