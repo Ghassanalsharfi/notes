6f5ddd54ac99e4f03b513c4d0a2e15a6b0fd6371
My project <g2036535@gmail.com>
2026-06-21 23:06:39 +0300

    Create NoteView

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:notes/widgets/note_view_body.dart';
 
 class NoteView extends StatelessWidget {
   const NoteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: const NoteViewBody(),
+    );
   }
 }
