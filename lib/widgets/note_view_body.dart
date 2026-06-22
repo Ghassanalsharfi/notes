3fd044b5288fa024787b855d1f99d2e9e5cc1c17
My project <g2036535@gmail.com>
2026-06-22 22:54:10 +0300

    #

--- lib/widgets/note_view_body.dart
+++ lib/widgets/note_view_body.dart
@@ -8,10 +8,39 @@ class NoteViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(children: const [SizedBox(height: 50), CustomAppBar()]),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.amberAccent,
+        borderRadius: BorderRadius.circular(16),
+      ),
       child: Column(
-        children: const [
-          SizedBox(height: 50,),
-          CustomAppBar(),],
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text('Flutter Note App'
+            ,style: TextStyle(
+              color: Colors.black,
+            ),),
+            subtitle: Text('Bulid yor Notes ' ,style: TextStyle(
+              color: Colors.black,
+            ),),
+            trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.black,)),
+          ),
+
+          Text('May 21 , 2026' ,style: TextStyle(
+              color: Colors.black,
+            ),)
+        ],
       ),
     );
   }
