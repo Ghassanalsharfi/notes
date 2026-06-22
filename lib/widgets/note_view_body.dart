4c9271ff7571222a192f751670da8f6f9a6057f0
My project <g2036535@gmail.com>
2026-06-22 23:21:08 +0300

    Add poppins font and update desgin of Note  Item

--- lib/widgets/note_view_body.dart
+++ lib/widgets/note_view_body.dart
@@ -8,7 +8,9 @@ class NoteViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.0),
-      child: Column(children: const [SizedBox(height: 50), CustomAppBar()]),
+      child: Column(
+        children: const [SizedBox(height: 50), CustomAppBar(), NoteItem()],
+      ),
     );
   }
 }
