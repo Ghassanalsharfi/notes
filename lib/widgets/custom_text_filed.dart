cb3646c7320909a96c17ad2a6492b80d7148b1c4
My project <g2036535@gmail.com>
2026-06-23 17:27:24 +0300

    Refactor CustomTextFiled to use TextField; update hintStyle color and remove unused validator logic

--- lib/widgets/custom_text_filed.dart
+++ lib/widgets/custom_text_filed.dart
@@ -1,32 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:notes/constants.dart';
 
 class CustomTextFiled extends StatelessWidget {
-   CustomTextFiled({this.hintText, this.obscureText=false, this.onChanged});
-  String ?hintText;
-  bool ?obscureText;
+  CustomTextFiled({this.hintText, this.obscureText = false, this.onChanged});
+  String? hintText;
+  bool? obscureText;
   Function(String)? onChanged;
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      obscureText: obscureText!,
-       validator: (value) {
-            if (value == null || value.isEmpty) {
-              return 'field is required';
-            }
-            return null;
-          },
-      onChanged: onChanged,
+    return TextField(
+      cursorColor: kPrimaryColor,
       decoration: InputDecoration(
+        border: buildBorder(),
+        focusedBorder: buildBorder(kPrimaryColor),
+        enabledBorder: buildBorder(),
         hintText: hintText,
         hintStyle: TextStyle(
-          color: Colors.white,
+          color: kPrimaryColor,
         ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.white),
-        ),
-        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
       ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([Color? color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: color ?? Colors.white),
     );
   }
 }
