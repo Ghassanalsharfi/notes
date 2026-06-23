f0461d9eb3a1520c7b2c7b7716732c82bddeb273
My project <g2036535@gmail.com>
2026-06-23 18:34:05 +0300

    create Note Model

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes/widgets/custom_app_bar.dart';
+import 'package:notes/widgets/custom_button.dart';
+import 'package:notes/widgets/custom_text_filed.dart';
 
 class EditNoteViewBody  extends StatelessWidget {
   const EditNoteViewBody ({super.key});
@@ -11,6 +13,14 @@ class EditNoteViewBody  extends StatelessWidget {
       child: Column(
         children: [SizedBox(height: 50),
           CustomAppBar(titel: 'Edit Note',icon: Icons.check,),
+          
+            SizedBox(height: 32,),
+        
+            CustomTextFiled(hintText: 'Titel',),
+            SizedBox(height: 16,),
+            CustomTextFiled(hintText: 'Content', maxLines: 5,),
+            SizedBox(height: 50,),
+             SizedBox(height: 32,),
         ],
       ),
     );
