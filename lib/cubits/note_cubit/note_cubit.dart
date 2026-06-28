84e7b23af27bd336efd1eca41b1c9b48437adabc
My project <g2036535@gmail.com>
2026-06-28 22:28:07 +0300

    Implement NoteCubit for state management and integrate it into NoteView for improved note handling

--- lib/cubits/note_cubit/note_cubit.dart
+++ lib/cubits/note_cubit/note_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes/constants.dart';
 import 'package:notes/model/note_model.dart';
 
 part 'note_state.dart';
 
 class NoteCubit extends Cubit<NoteState> {
   NoteCubit() : super(NoteInitial());
+  fetchAllNote() async {
+    emit(NoteLoading());
+
+    try {
+      var noteBox = Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> notes = noteBox.values.toList();
+      emit(NoteSuccess(notes));
+    } catch (e) {
+      emit(NoteFailure(e.toString()));
+    }
+  }
 }
+ 
--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/note_cubit/note_cubit.dart';
 import 'package:notes/widgets/add_note_bottom_sheet.dart';
 import 'package:notes/widgets/note_view_body.dart';
 
@@ -7,25 +9,27 @@ class NoteView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Color.fromARGB(255, 0, 251, 255),
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadiusGeometry.circular(16)
-            ),
-            context: context,
-            builder: (context) {
-              return AddNoteBottomShet();
-            },
-          );
-        },
-        child: const Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => NoteCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Color.fromARGB(255, 0, 251, 255),
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusGeometry.circular(16),
+              ),
+              context: context,
+              builder: (context) {
+                return AddNoteBottomShet();
+              },
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: const NoteViewBody(),
       ),
-      body: const NoteViewBody(),
     );
   }
 }
-
